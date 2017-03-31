f68fb54555060559ac0e9bd2c90a69afe9b52a08
Vasil <vasil.yoshev@gmail.com>
2017-03-31 20:05:57 +0300

    same

---
+++ src/ui/Image.java
@@ -0,0 +1,237 @@
+package ui;
+
+public final class Image {
+
+	// Game window
+	private static final String gameBG = "background.png";
+	private static final String result = "result.png";
+	private static final String theme = "change theme.png";
+	private static final String themePressed = ""; // TODO
+	private static final String themeHover = ""; // TODO
+	private static final String newGame = "new game.png";
+	private static final String newGamePressed = ""; // TODO
+	private static final String newGameHover = ""; // TODO
+	private static final String grid = "grid450.png";
+	private static final String crossImg = "x150.png";
+	private static final String noughtImg = "o150.png";
+	
+
+	// Menu windows
+	private static final String menuBG = "menubg.png";
+	private static final String TTTMain = "bgTheme/main/ticTacToe.png";
+	private static final String playLarge = "bgTheme/couch/play.png";
+	private static final String playLargePressed = "bgTheme/couch/playPressed.png";
+	private static final String playLargeHover = "bgTheme/single/playHover.png";
+	private static final String singlePlayer = "bgTheme/main/single.png";
+	private static final String singlePlayerPressed = "bgTheme/main/singlePressed.png";
+	private static final String singlePlayerHover = "bgTheme/main/singleHover.png";
+	private static final String multiPlayer = "bgTheme/main/multi.png";
+	private static final String multiPlayerPressed = "bgTheme/main/multiPressed.png";
+	private static final String multiPlayerHover = "bgTheme/main/multiHover.png";
+	private static final String couchCoOp = "bgTheme/main/couch.png";
+	private static final String couchCoOpPressed = "bgTheme/main/couchPressed.png";
+	private static final String couchCoOpHover = "bgTheme/main/couchHover.png";
+	private static final String playSmall = "bgTheme/multi/play.png";
+	private static final String playSmallPressed = "bgTheme/multi/playPressed.png";
+	private static final String playSmallHover = ""; // TODO
+	// instances
+	
+	private Image() {
+	}
+
+	/**
+	 * @return the gamebg
+	 */
+	public static String getGamebg() {
+		return gameBG;
+	}
+
+	/**
+	 * @return the result
+	 */
+	public static String getResult() {
+		return result;
+	}
+
+	/**
+	 * @return the theme
+	 */
+	public static String getTheme() {
+		return theme;
+	}
+
+	/**
+	 * @return the themePressed
+	 */
+	public static String getThemepressed() {
+		return themePressed;
+	}
+
+	/**
+	 * @return the themehover
+	 */
+	public static String getThemehover() {
+		return themeHover;
+	}
+
+	/**
+	 * @return the newgame
+	 */
+	public static String getNewgame() {
+		return newGame;
+	}
+
+	/**
+	 * @return the newgamepressed
+	 */
+	public static String getNewgamepressed() {
+		return newGamePressed;
+	}
+
+	/**
+	 * @return the newgamehover
+	 */
+	public static String getNewgamehover() {
+		return newGameHover;
+	}
+
+	/**
+	 * @return the grid
+	 */
+	public static String getGrid() {
+		return grid;
+	}
+
+	/**
+	 * @return the crossimg
+	 */
+	public static String getCrossImg() {
+		return crossImg;
+	}
+
+	/**
+	 * @return the noughtimg
+	 */
+	public static String getNoughtImg() {
+		return noughtImg;
+	}
+
+	/**
+	 * @return the menubg
+	 */
+	public static String getMenubg() {
+		return menuBG;
+	}
+
+	/**
+	 * @return the playlarge
+	 */
+	public static String getPlaylarge() {
+		return playLarge;
+	}
+
+	/**
+	 * @return the playlargepressed
+	 */
+	public static String getPlaylargepressed() {
+		return playLargePressed;
+	}
+
+	/**
+	 * @return the playlargehover
+	 */
+	public static String getPlaylargehover() {
+		return playLargeHover;
+	}
+
+	/**
+	 * @return the tttmain
+	 */
+	public static String getTttmain() {
+		return TTTMain;
+	}
+
+	/**
+	 * @return the singleplayer
+	 */
+	public static String getSingleplayer() {
+		return singlePlayer;
+	}
+
+	/**
+	 * @return the singleplayerpressed
+	 */
+	public static String getSingleplayerpressed() {
+		return singlePlayerPressed;
+	}
+
+	/**
+	 * @return the singleplayerhover
+	 */
+	public static String getSingleplayerhover() {
+		return singlePlayerHover;
+	}
+
+	/**
+	 * @return the multiplayer
+	 */
+	public static String getMultiplayer() {
+		return multiPlayer;
+	}
+
+	/**
+	 * @return the multiplayerpressed
+	 */
+	public static String getMultiplayerpressed() {
+		return multiPlayerPressed;
+	}
+
+	/**
+	 * @return the multiplayerhover
+	 */
+	public static String getMultiplayerhover() {
+		return multiPlayerHover;
+	}
+
+	/**
+	 * @return the couchcoop
+	 */
+	public static String getCouchcoop() {
+		return couchCoOp;
+	}
+
+	/**
+	 * @return the couchcooppressed
+	 */
+	public static String getCouchcooppressed() {
+		return couchCoOpPressed;
+	}
+
+	/**
+	 * @return the couchcoophover
+	 */
+	public static String getCouchcoophover() {
+		return couchCoOpHover;
+	}
+
+	/**
+	 * @return the playsmall
+	 */
+	public static String getPlaysmall() {
+		return playSmall;
+	}
+
+	/**
+	 * @return the playsmallpressed
+	 */
+	public static String getPlaysmallpressed() {
+		return playSmallPressed;
+	}
+
+	/**
+	 * @return the playsmallhover
+	 */
+	public static String getPlaysmallhover() {
+		return playSmallHover;
+	}
+}
